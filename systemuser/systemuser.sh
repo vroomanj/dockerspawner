82bee977a5364470d9d29c196b2d7befbd0a8063
Justin Vrooman <vroomanj@gmail.com>
2016-01-14 19:47:30 -0500

    hack it to work

--- systemuser/systemuser.sh
+++ systemuser/systemuser.sh
@@ -2,7 +2,7 @@
 
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL $USER
-sudo -E -u $USER jupyterhub-singleuser \
+sudo -E -u $USER /usr/local/bin/jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
